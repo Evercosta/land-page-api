243384e742f4ec35ae85078cdd5effbb7b2674c3
Everton Costa <evercosta@gmail.com>
2020-07-14 13:36:11 -0300

    Send content

---
+++ src/app.js
@@ -0,0 +1,36 @@
+'use strict'; // ajuda o slint a checar erros
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const monggose = require('mongoose');
+
+const app = express();
+
+// Carrega os Models
+// const Webhook = require('./models/webhook');
+
+// Carrega as Rotas
+const indexRoute = require('./routes/index-route');
+
+// converter o body com o bodyParser
+app.use(bodyParser.json({
+    limit: '10mb'
+}));
+// codifica a url
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Habilita o CORS
+app.use(function(req, res, next){
+    res.header('Access-control-Allow-Origin', '*');
+    res.header(
+        'Access-control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header(
+        'Access-control-Allow-Methods', 
+        'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/', indexRoute);
+
+module.exports = app;
