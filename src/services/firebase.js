13004044570b3cae1d71bf75bf0df9028b38fed4
Everton Costa <evercosta@gmail.com>
2020-07-17 14:06:28 -0300

    change gitgnore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,8 @@
 const admin = require('firebase-admin');
-let serviceAccount = require('../../keyfile.json');
+let serviceAccount = require('../../key/keyfile.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
-
-// let db = admin.firestore();
 
 module.exports = admin;
