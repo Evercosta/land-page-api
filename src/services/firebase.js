d48b318abb48123f9dbb1a0832319a673d60711b
Everton Costa <evercosta@gmail.com>
2020-07-17 15:03:56 -0300

    Install dotenv, config and add file, del keyfile

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,23 @@
 const admin = require('firebase-admin');
-let serviceAccount = require('../../key/keyfile.json');
+// let serviceAccount = require('../../key/keyfile.json');
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        type: process.env.FB_TYPE,
+        project_id: process.env.FB_PROJECT_ID,
+        private_key_id: process.env.FB_PRIVATE_KEY_ID,
+        private_key: process.env.FB_PRIVATE_KEY,
+        client_email: process.env.FB_CLIENT_EMAIL,
+        client_id: process.env.FB_CLIENT_ID,
+        auth_uri: process.env.FB_AUTH_URI,
+        token_uri: process.env.FB_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FB_CLIENT_X509_CERT_URL
+    })
 });
 
 module.exports = admin;
