6310784195029e5ac4f1e77788bbfd4b86cc2580
Everton Costa <evercosta@gmail.com>
2020-07-18 19:49:29 -0300

    Remove test on index

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -2,24 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-const emailService = require('../services/send-email');
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
 
 router.get('/', (req, res, next)=>{
-    const nome = 'Nome Teste 4';
-
-    console.log('Teste from: ', process.env.SEND_EMAIL_USER);
-    console.log('Teste to: ', process.env.SEND_EMAIL_TO);
-    console.log('Teste subject: ', 'Atenção, novo Lead adicionado');
-    console.log('Teste html: ', '<p><strong>'+nome+'</strong> acabou de se cadastrar no sistema.</p>');
-
-    emailService.send(
-        process.env.SEND_EMAIL_TO, 
-        'Atenção, novo Lead adicionado',
-        '<p><strong>'+nome+'</strong> acabou de se cadastrar no sistema.</p>'
-    );
     res.status(200).send({
         title: "Node LandPage API",
         version: "1.0.0"
