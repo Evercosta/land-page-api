7b7b08cdde116b34a8bd6875f356487cc3ff2d95
Everton Costa <evercosta@gmail.com>
2020-07-18 18:20:06 -0300

    Send mail test

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -2,8 +2,18 @@
 
 const express = require('express');
 const router = express.Router();
+const emailService = require('../services/send-email');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 router.get('/', (req, res, next)=>{
+    const nome = 'Nome Teste 2';
+    emailService.send(
+        process.env.SEND_EMAIL_TO, 
+        'Atenção, novo Lead adicionado',
+        `<p><strong>${nome}</strong> acabou de se cadastrar no sistema.</p>`
+    );
     res.status(200).send({
         title: "Node LandPage API",
         version: "1.0.0"
