38026755a5b5baf5fb8b05e58dbfa5428de53ab2
Everton Costa <evercosta@gmail.com>
2020-07-18 19:20:20 -0300

    Send mail test 3

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -8,7 +8,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 router.get('/', (req, res, next)=>{
-    const nome = 'Nome Teste 2';
+    const nome = 'Nome Teste 3';
+    
+    console.log('Teste from: ', process.env.SEND_EMAIL_USER);
+    console.log('Teste to: ', process.env.SEND_EMAIL_TO);
+    console.log('Teste subject: ', 'Atenção, novo Lead adicionado');
+    console.log('Teste html: ', '<p><strong>'+nome+'</strong> acabou de se cadastrar no sistema.</p>');
+
     emailService.send(
         process.env.SEND_EMAIL_TO, 
         'Atenção, novo Lead adicionado',
